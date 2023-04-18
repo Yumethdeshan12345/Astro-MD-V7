19762f6b8ef3d73e4f7e4ed87db4d033bbdf5649
Cipher <109347753+Yumethdeshan12345@users.noreply.github.com>
2023-04-19 00:27:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀ
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? 'REPLIT' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=0nIxERSC#oqfTPQjNFoEsnJr9bwZFvaIv_l0X-5ViXr0-bnkeVck' : process.env.SESSION_ID
 };
